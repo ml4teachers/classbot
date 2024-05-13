f7a828f6ac49a0fd72ea2a36c265922f94e35a5c
Thomas <tho_zurfluh@me.com>
2024-05-13 21:42:36 +0200

    gpt4o

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,29 +1,21 @@
-import Anthropic from '@anthropic-ai/sdk';
-import { AnthropicStream, StreamingTextResponse } from 'ai';
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
  
-// Create an Anthropic API client (that's edge friendly)
-const anthropic = new Anthropic({
-  apiKey: process.env.ANTHROPIC_API_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
 });
  
-// IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
  
 export async function POST(req: Request) {
-  // Extract the `prompt` from the body of the request
   const { messages } = await req.json();
  
-  // Ask Claude for a streaming chat completion given the prompt
-  const response = await anthropic.messages.create({
-    messages,
-    model: 'claude-3-haiku-20240307',
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4o',
     stream: true,
-    max_tokens: 400,
+    messages,
   });
  
-  // Convert the response into a friendly text-stream
-  const stream = AnthropicStream(response);
- 
-  // Respond with the stream
+  const stream = OpenAIStream(response);
   return new StreamingTextResponse(stream);
 }
